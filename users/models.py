73c692a57a1185a1f3d7890756814f12e82a255e
Pavel2232 <pavelmazikinvladimirovich@gmail.com>
2023-11-27 12:51:58 +0300

    style(users.models):
    editing quotes

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 
 class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=200, verbose_name="Имя")
-    email = models.EmailField(max_length=255, null=False,unique=True, verbose_name="Почта")
+    first_name = models.CharField(max_length=200, verbose_name='Имя')
+    email = models.EmailField(max_length=255, null=False,unique=True, verbose_name='Почта')
     is_active = models.BooleanField(default=True)
     data_joined = models.DateTimeField(auto_now_add=True)
     is_superuser = models.BooleanField(default=False)
@@ -25,8 +25,8 @@ class User(AbstractBaseUser):
         return True
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.email
